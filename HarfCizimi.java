bbd087e29475d65214b936c90157d7949cb87d50
thetise <107493797+thetise@users.noreply.github.com>
2023-03-07 11:06:41 +0300

    Add files via upload

---
+++ HarfCizimi.java
@@ -0,0 +1,29 @@
+package ArrayHomeworks;
+
+public class HarfCizimi {
+    public static void main(String[] args) {
+
+        String[][] arr = new String[7][4];
+
+        for(int i = 0; i < arr.length; i++){
+
+            for(int j = 0; j < arr[i].length; j++){
+
+                if(i == 0 || i == 3 || i == 6){
+                    System.out.print("*");
+                }
+                else{
+                    if(j == 0 || j == 3){
+
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+                }
+                System.out.print(" ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
